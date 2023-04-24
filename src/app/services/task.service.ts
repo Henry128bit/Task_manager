3631430d34e68e61eaa2e10232d982e99cb4588b
Sergey Kamensky <henry.128bit@gmail.com>
2023-04-24 22:04:49 +0400

    Observable and json-server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import {Task} from '../Task';
-import {TASKS} from '../mock-tasks';
+// import {TASKS} from '../mock-tasks';
+
+// Server
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  // constructor() { }
+
+  // getTasks(): Observable<Task[]> {
+  //   const tasks = of(TASKS);
+  //   return tasks;
+  // }
+
+  //Server
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
